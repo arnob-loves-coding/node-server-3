5680430b2a2a29af8a3398a2ea021a5ec904cdf5
arnob-loves-coding <arnobroy-13-2018623891@eng.du.ac.bd>
2021-11-01 16:50:19 +0600

    s

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // importt
 const express = require("express");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient } = require("mongodb");
